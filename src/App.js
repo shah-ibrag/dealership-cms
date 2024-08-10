77482f566055ee37ccc2eda05f48f818474dcc08
Shawn Ibragimov <shah.ibrag@gmail.com>
2024-08-09 19:15:45 -0500

    Header + navigation created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";  
+
+
+function ToggleButton() {
+  const buttonStyle =   {
+    backgroundColor: "rgb(215, 85, 15",
+    border: "none",
+    fontSize: "40px",
+    height: "200px",
+    width: "200px",
+  }
+
+  return (
+    <button style={buttonStyle}>Styled Button</button>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToggleButton />
+    </>
   );
 }
 
